07c23fa770026388a27f7209c51a7bc7ff7182d6
Sean Og <seanogmc@yahoo.com>
2025-11-06 23:25:55 +0000

    Add Card class
    Basic functionality developed:
    - Constructor & Randomised Constructor
    - toString() for printing generated Cards

---
+++ Objects/Card.js
@@ -0,0 +1,26 @@
+import { SUITS, VALUES } from "./Constants";
+
+export default class Card {
+  constructor(suit, value) {
+    if (!SUITS.includes(suit)) {
+      throw new Error(`Invalid Suit ${suit}`);
+    }
+    if (!VALUES.includes(value)) {
+      throw new Error(`Invalid Value ${value}`);
+    }
+
+    this.suit = suit;
+    this.value = value;
+  }
+
+  static constructRandomCard() {
+    const suit = SUITS[Math.floor(Math.random() * SUITS.length)];
+    const value = VALUES[Math.floor(Math.random() * VALUES.length)];
+
+    return new Card(suit, value);
+  }
+
+  toString() {
+    return `${this.value} of ${this.suit}`;
+  }
+}
