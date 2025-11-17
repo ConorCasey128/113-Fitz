7065ee9c2f3a1560c47f8ad152b21b16cdab7adc
Sean Og <seanogmc@yahoo.com>
2025-11-17 16:01:48 +0000

    Remove big card from middle of main screen.

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-import Card from "./Objects/Card.js";
-import Deck from "./Objects/Deck.js";
 import { VALUES } from "./Objects/Constants.js";
 
 const values = [...VALUES];
 const shuffleList = document.getElementById("shuffleList");
 const shuffleBtn = document.getElementById("shuffle-btn");
-const cardContainer = document.getElementById("card-container");
 
 function render() {
   shuffleList.textContent = values.join(", ");
-
-  // Creating dummy card object to test new props
-  const card1 = new Card("spades", "9");
-  const code = card1.getCode();
-
-  renderCardImage(card1);
-
-  console.log(code);
 }
 
 function shuffle() {
@@ -27,23 +16,6 @@ function shuffle() {
     values[j] = k;
   }
   render();
-}
-
-function getCardImagePath(card) {
-  const code = card.getCode();
-  return `Images/Deck/poker-super1/${code}.svg`;
-}
-
-export function renderCardImage(card) {
-  cardContainer.innerHTML = "";
-
-  const img = document.createElement("img");
-
-  img.src = getCardImagePath(card);
-
-  img.classList.add("card-image");
-
-  cardContainer.appendChild(img);
 }
 
 render();
